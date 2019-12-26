15fd932dead2f05e711c016c587787f3eaa3a8ef
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-26 08:18:02 +0200

    added many to many relationship

---
+++ src/main/java/com/springframework/recipe/domain/Category.java
@@ -0,0 +1,44 @@
+package com.springframework.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * @author Ibrahim
+ * @created 26/12/2019 - 7:55 AM
+ * @project spring5-recipe-app
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
--- src/main/java/com/springframework/recipe/domain/Recipe.java
+++ src/main/java/com/springframework/recipe/domain/Recipe.java
@@ -9,22 +9,32 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
     private String source;
     private String url;
+    private String directions;
+
     //Default Behavior of EnumType is ORDINAL : means the index of each entry of Enum (1 or 2 or ...)
     @Enumerated(value = EnumType.STRING)
-    private String directions;
     private Difficulty difficulty;
+
     @Lob
     private byte[] image;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -120,5 +130,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
